61c992c0fd96d9072e8b71bac2bce83308035e00
Purplelops <purplelops@gmail.com>
2022-11-14 23:17:15 +0100

    Update is_prime.py

--- is_prime.py
+++ is_prime.py
@@ -1,4 +1,4 @@
-from math import floor
+import math
 
 # Ask the user for input and convert it to an int
 num = int(input("Input the number you want to check: "))
@@ -18,7 +18,7 @@ while True:
         break;
     else:
         # Check if the number is still less than half of the input number
-        if divisor == floor(num / 2):
+        if divisor == math.floor(num / 2):
             print("Number is prime!")
             break;
         else:
