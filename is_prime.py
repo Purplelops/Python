eaad44df9e3a6d7310186a766d275a714a94d85c
Purplelops <purplelops@gmail.com>
2022-11-14 23:07:34 +0100

    Made roughly twice as fast by adding two to the divisor every iteration

--- is_prime.py
+++ is_prime.py
@@ -3,7 +3,12 @@ import math
 # Ask the user for input and convert it to an int
 num = int(input("Input the number you want to check: "))
 
-divisor = 2
+# Check if number is divisible by two
+if num % 2 == 0:
+    print("Number is not prime! It is divisible by 2")
+    exit()
+
+divisor = 3
 
 # Loop over all numbers less than half of the number the user picked
 while True:
@@ -17,5 +22,6 @@ while True:
             print("Number is prime!")
             break;
         else:
-            # Add one to the divisor and repeat
-            divisor += 1
+            # Add two to the divisor and repeat
+            # I add two, because any number divisible by an even number must be even
+            divisor += 2
